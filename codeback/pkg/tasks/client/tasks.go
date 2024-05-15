71ef917ca0b02081ed625600a0ab28eb0fce9712
Virgil <virgiling7@gmail.com>
2024-05-15 20:07:36 +0800

    fix: bug in print command

--- codeback/pkg/tasks/client/tasks.go
+++ codeback/pkg/tasks/client/tasks.go
@@ -3,7 +3,7 @@ package client
 import "os/exec"
 
 func PrintCode(filename string, printer string) error {
-	cmd := exec.Command("cat " + filename + " | paps --fonts=10 --top-margin=10 --bottom-margin=10 --lpi=10 | lp -d " + printer)
+	cmd := exec.Command("cat " + filename + " | paps --font=10 --top-margin=10 --bottom-margin=10 --lpi=10 | lp -d " + printer)
 	err := cmd.Run()
 	return err
 }
